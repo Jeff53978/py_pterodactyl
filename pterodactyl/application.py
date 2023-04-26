d9cd3325d96d4dc1286914a3961e46d0edc199ba
Jeff <99515656+Jeff53978@users.noreply.github.com>
2023-04-26 12:46:38 +0200

    Delete application.py

--- pterodactyl/application.py
+++
@@ -1,9 +0,0 @@
-import httpx
-
-from .errors import AuthenticationError
-
-class ApplicationClient:
-    def __init__(self, panel_url, api_key):
-        self.panel_url = panel_url
-        self.api_key = api_key
-        self.client = httpx.Client()
